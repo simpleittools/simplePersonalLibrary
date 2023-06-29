63c32b828371bb125ca5f769a6ae1eed10ed6eea
Ryan Mooney <wm.ryan.mooney@gmail.com>
2023-06-29 09:52:49 -0800

    Add InfoLog logger and UseCache boolean to the global app config in config.go

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,7 +1,12 @@
 package config
 
-import "html/template"
+import (
+	"html/template"
+	"log"
+)
 
 type AppConfig struct {
+	UseCache      bool
 	TemplateCache map[string]*template.Template
+	InfoLog       *log.Logger
 }
