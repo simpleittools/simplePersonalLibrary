a4aa3d85aa0a7e4edb8daa00f5e3d4c3f03d1e17
Ryan Mooney <wm.ryan.mooney@gmail.com>
2023-07-08 19:15:10 -0800

    Add validation functionality for Min Length and add loop to check for all required form items

--- internal/forms/forms.go
+++ internal/forms/forms.go
@@ -1,8 +1,10 @@
 package forms
 
 import (
+	"fmt"
 	"net/http"
 	"net/url"
+	"strings"
 )
 
 // Form creates a custom Form struct, and it embeds a url.Values object
@@ -19,11 +21,31 @@ func New(data url.Values) *Form {
 	}
 }
 
+// Required will range through the fields variable
+func (f *Form) Required(fields ...string) {
+	for _, field := range fields {
+		value := f.Get(field)
+		if strings.TrimSpace(value) == "" {
+			f.Errors.Add(field, "This field cannot be blank")
+		}
+	}
+}
+
 // Has checks if the form field is in POST and not empty
 func (f *Form) Has(field string, r *http.Request) bool {
 	x := r.Form.Get(field)
 	if x == "" {
 		f.Errors.Add(field, "This field cannot be blank")
+		return false
+	}
+	return true
+}
+
+// MinLength is the minimum length validator
+func (f *Form) MinLength(field string, length int, r *http.Request) bool {
+	x := r.Form.Get(field)
+	if len(x) < length {
+		f.Errors.Add(field, fmt.Sprintf("This field must be at least %d characters long.", length))
 		return false
 	}
 	return true
