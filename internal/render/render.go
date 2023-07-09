146a16ac6e2d78d162f0f3a0678bb34fa52b2dcb
Ryan Mooney <wm.ryan.mooney@gmail.com>
2023-07-08 20:05:25 -0800

    Build end user notifications

--- internal/render/render.go
+++ internal/render/render.go
@@ -18,6 +18,9 @@ func NewTemplates(a *config.AppConfig) {
 }
 
 func AddDefaultData(templateData *models.TemplateData, r *http.Request) *models.TemplateData {
+	templateData.Flash = app.Session.PopString(r.Context(), "flash")
+	templateData.Warning = app.Session.PopString(r.Context(), "error")
+	templateData.Error = app.Session.PopString(r.Context(), "warning")
 	templateData.CSRFToken = nosurf.Token(r)
 	return templateData
 }
